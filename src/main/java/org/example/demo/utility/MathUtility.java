15ea9003fd0ad586e0d14a339b9df948ca410e2c
MuniTeja8197 <53068672+MuniTeja8197@users.noreply.github.com>
2023-06-24 14:01:30 -0500

    Initial commit

---
+++ src/main/java/org/example/demo/utility/MathUtility.java
@@ -0,0 +1,12 @@
+package org.example.demo.utility;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class MathUtility {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
+
